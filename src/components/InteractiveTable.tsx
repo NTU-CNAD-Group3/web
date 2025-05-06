6024bb9eff6682fe6005e575bd63bf4302d202dd
chuntaopeng <pengtaiwan6517@gmail.com>
2025-05-06 14:58:46 +0800

    fixing unused variables

--- src/components/InteractiveTable.tsx
+++ src/components/InteractiveTable.tsx
@@ -16,7 +16,6 @@ import {
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Textarea } from '@/components/ui/textarea';
 
 interface Item {
   id: string;
